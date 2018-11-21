f324a0ec909681992be7f69cbce1d0176989b07e
Samed Karakus <karakusnavy@gmail.com>
2018-11-21 17:09:49 +0300

    updated

---
+++ StoryTR/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace StoryTR
+{
+    [Activity(Label = "@string/app_name", Theme = "@android:style/Theme.Material.Light.NoActionBar", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button btn,btn3;
+        LinearLayout btn2;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);            
+            SetContentView(Resource.Layout.activity_main);
+            btn = FindViewById<Button>(Resource.Id.button2);
+            btn3 = FindViewById<Button>(Resource.Id.button1);
+            btn2 = FindViewById<LinearLayout>(Resource.Id.linearLayout2);
+            btn2.Click += Btn2_Click;
+            btn.Click += Btn_Click;            
+            btn3.Click+= Btn2_Click;
+        }
+
+        private void Btn_Click(object sender, System.EventArgs e)
+        {
+            btn3.Text = "Thanks for click";                        
+        }
+
+        private void Btn2_Click(object sender, System.EventArgs e)
+        {
+            btn.Text = "Thanks for click";
+        }
+    }
+}
---
+++ StoryTR/obj/Debug/MonoAndroid81/android/src/md5e505a3beb786d203d0c45d06de84b560/MainActivity.java
@@ -0,0 +1,47 @@
+package md5e505a3beb786d203d0c45d06de84b560;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("StoryTR.MainActivity, StoryTR", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("StoryTR.MainActivity, StoryTR", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
